cb749981776d558ce38c6b773c19ec52e39fbefa
Trần Phương Quang Huy <100473444+KengG1M@users.noreply.github.com>
2025-03-21 11:01:29 +0700

    goroutine practice 2

--- tutorial/practice/channel/channel.go
+++ tutorial/practice/channel/channel.go
@@ -1,5 +1,14 @@
 package main
 
+/*
+Channel là cơ chế giao tiếp an toàn và đồng bộ giữa các goroutine
+
+Bạn có thể hình dung Channel như một đường ống pipe mà qua ddos các goroutine trao đổi dữ liệu với nhau
+
+Việc gửi và nhận dữ liệu trên channel có thể chặn (blocking)
+goroutine cho đến khi dữ liệu sẵn sàng, giúp tránh tình trạng
+race-condition(tranh chấp dữ liệu) mà không cần khóa (mutex) phức tạp
+*/
 func main() {
 
 }
